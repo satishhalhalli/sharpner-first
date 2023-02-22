cdf6c652544d05380999a565731f1b60d071b0ac
satishhalhalli <satishhalhalli7484@gmail.com>
2023-02-22 23:00:02 +0530

    queyselect

--- App.js
+++ App.js
@@ -4,3 +4,14 @@ headerTitle.style.borderBottom="solid 3px #000 ";
 var titles=document.querySelector(".title");
 titles.style.fontWeight='bold';
 titles.style.color='green';
+
+
+const thirdItem=document.querySelector('#items li:nth-child(3)');
+thirdItem.style.backgroundColor='green';
+// const thirdListItem = document.querySelector('#items li:nth-child(3)');
+// thirdListItem.style.backgroundColor = 'green';
+ const listItems=document.querySelectorAll('#items li');
+ for(let i=0;i<listItems.length;i++)
+ {
+    listItems[i].style.fontWeight='bold';
+ }
