edbbe70ac4b9b9e74d3cd681e4afa3e5050f75d5
satishhalhalli <satishhalhalli7484@gmail.com>
2023-02-22 21:18:32 +0530

    addinhhtmlFile

--- App.js
+++ App.js
@@ -1,1 +1,6 @@
-console.log("welcome to webdevelopment");
+var headerTitle=document.getElementById("header-title");
+headerTitle.style.borderBottom="solid 3px #000 ";
+
+var titles=document.querySelector(".title");
+titles.style.fontWeight='bold';
+titles.style.color='green';
