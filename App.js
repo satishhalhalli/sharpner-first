f40d15275cc3497719391a2b93b79beaea55b0f8
satishhalhalli <satishhalhalli7484@gmail.com>
2023-02-23 11:30:39 +0530

    cretingNewDivs

--- App.js
+++ App.js
@@ -1,45 +1,22 @@
-// var headerTitle=document.getElementById("header-title");
-// headerTitle.style.borderBottom="solid 3px #000 ";
-
-// var titles=document.querySelector(".title");
-// titles.style.fontWeight='bold';
-// titles.style.color='green';
 
 
-// const thirdItem=document.querySelector('#items li:nth-child(3)');
-// thirdItem.style.backgroundColor='green';
-// const thirdListItem = document.querySelector('#items li:nth-child(3)');
-// thirdListItem.style.backgroundColor = 'green';
-//  const listItems=document.querySelectorAll('#items li');
-//  for(let i=0;i<listItems.length;i++)
-//  {
-//     listItems[i].style.fontWeight='bold';
-//  }
-// 
+const helloWorldDiv = document.createElement('div');
+helloWorldDiv.setAttribute('id', 'hello-world');
+helloWorldDiv.setAttribute('class', 'card card-body');
 
-// const newItem=document.createElement('li');
-// newItem.textContent='New Item';
-// document.querySelector('#items').appendChild(newItem);
+helloWorldDiv.setAttribute('class', 'title');
+var newDivText=document.createTextNode('Hello world');
+helloWorldDiv.appendChild(newDivText);
 
 
 
-//this is by tagname
-// const allItems = document.getElementsByTagName('li');
-// const newItemIndex = Array.from(allItems).findIndex(item => item.textContent === 'New Item');
-// allItems[newItemIndex].textContent = 'Edited Item';
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+container.insertBefore(helloWorldDiv,h1);
 
 
-//by class name
-// newItem.classList.add('new-class');
-// const itemsWithNewClass = document.getElementsByClassName('new-class');
-// itemsWithNewClass[0].textContent = 'Edited Item';
-
-
-const listItems=document.querySelectorAll('#items li');
-listItems[1].style.color='green';
-
-var odd=document.querySelectorAll('#items li:nth-child(odd)');
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='green';
-}
+const item1 = document.querySelector('#items li:first-child');
+const helloDiv = document.createElement('div');
+helloDiv.setAttribute('class', 'hello');
+helloDiv.textContent = 'Hello';
+item1.parentNode.insertBefore(helloDiv, item1);
